faf23523e88b0a01a1fbb58ec59a7d6066b98b31
Dominik <D.Paucic.Inf21@stud.bbbaden.ch>
2022-04-25 15:18:52 +0200

    rinkeby test

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,4 +1,4 @@
-const contractAddress = "YOUR CONTRACT ADDRESS"; //Replace with your own contract address
+const contractAddress = "0x509A3ffaB2A9cb3C9c0F84b358be21643893F46A"; //Replace with your own contract address
 const chain = 'rinkeby'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
