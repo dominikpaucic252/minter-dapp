3ce9809aa7bedcf5bf67bfb9bafe63690150b2b5
Dominik <D.Paucic.Inf21@stud.bbbaden.ch>
2022-05-04 19:25:23 +0200

    updated

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,9 +1,9 @@
 const contractAddress = "0x63d806AdCF3516bbd04ae34bd44ddACa86E7C4D7"; //Replace with your own contract address
 const chain = 'polygon'; // rinkeby or polygon
 
-const welcome_h1 = "Welcome to the CodeCats NFT Project!!";
+const welcome_h1 = "Welcome to the Silly Ghosts NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
-const welcome_p = 'The CodeCats NFT Project is a decentralized, open-source project that aims to demonstrate how to develope and launch your own NFT Collection. Follow step by step on the <a href="https://youtube.com/codestackr"  target="_blank" rel="noopener noreferrer">codeSTACKr</a> YouTube channel.';
+const welcome_p = 'The Silly Ghosts NFT project is a unique digital collection, on the Polygon Blockchain. 10,000 ghosts have been automatically generated using an algorithm, each ghost is unique, with different characteristics and rarities.';
 const h1_presale_coming_soon = "NFT Drop Coming Soon!!";
 const h1_presale_mint = "Pre-Sale Minting Open!!";
 const h1_public_mint = "Public Minting Open!!";
