21ecc0e590dfa3171ea9721645713eaaeb7d240f
Dominik <D.Paucic.Inf21@stud.bbbaden.ch>
2022-05-04 19:12:24 +0200

    updated

--- frontend/js/constants.js
+++ frontend/js/constants.js
@@ -1,5 +1,5 @@
-const contractAddress = "0xe047a4ad37013b2BB9E0eC7E624bB1Aa438DCdbc"; //Replace with your own contract address
-const chain = 'rinkeby'; // rinkeby or polygon
+const contractAddress = "0x63d806AdCF3516bbd04ae34bd44ddACa86E7C4D7"; //Replace with your own contract address
+const chain = 'polygon'; // rinkeby or polygon
 
 const welcome_h1 = "Welcome to the Silly Ghost Group NFT Project!!";
 const welcome_h2 = "Connect to MetaMask to Get Started";
